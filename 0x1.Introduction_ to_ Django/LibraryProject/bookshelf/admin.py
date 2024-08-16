31e9e94e7c0896fb7fb29d1986b0f9b19f3db9e7
jim <jim19rogers@gmail.com>
2024-08-16 17:06:10 +0300

    admin planel

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
