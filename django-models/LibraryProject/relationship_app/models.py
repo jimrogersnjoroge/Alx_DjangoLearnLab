86025db00c121b7fe7d8d74ea11cf1547fea619c
jim <jim19rogers@gmail.com>
2024-08-22 13:15:16 +0300

    Add relationship_app with models and query samples

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -28,3 +26,4 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
