da941dbd399791077930fafb188410a95ac6f90e
jim <jim19rogers@gmail.com>
2024-08-22 14:33:45 +0300

    Added function-based and class-based views with templates

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
