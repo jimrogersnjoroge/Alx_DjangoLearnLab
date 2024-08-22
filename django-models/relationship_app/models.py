93967ca891f402ee4fa2f196d1bbc3218471a6ee
jim <jim19rogers@gmail.com>
2024-08-22 14:47:00 +0300

    Added function-based and class-based views with templates and detail view modules included

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,31 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.db import models
 
+# Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+    
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author}"  
+
+#Extending Book Model with Custom Permissions
+    class Meta(models.Model):
+        Permissions_Choices =(
+            ('can_add_book', 'can_add_book'),
+            ('can_change_book', 'can_change_book'),
+            ('can_delete_book', 'can_delete_book'),
+
+        )
+    permissions = models.CharField(max_length=50,  choices='Permissions_Choices')
+    meta = models.TextField()
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.permissions}'
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
-        return self.name
+        return self.name   
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarians')
 
     def __str__(self):
         return self.name
+    
+#Extending User Model with a UserProfile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    Role_Choices =(
+        ('Admin','Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    
+    )
+
+role = models.CharField(max_length=50,  choices='Role_Choices')
+userprofile = models.TextField()
+
+def __str__(self):
+    return f'{self.user.username} - {self.role}'
+
+#Signal to automatically create a UserProfile when a new User is created
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,14 @@
+# relationship_app/views.py
 from django.shortcuts import render
-
-# Create your views here.
-from .models import Book, Library
 from django.views.generic.detail import DetailView
+from .models import Book, Library  # Import Library here
+
+# Function-based view for listing all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
+# Class-based view for displaying details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
