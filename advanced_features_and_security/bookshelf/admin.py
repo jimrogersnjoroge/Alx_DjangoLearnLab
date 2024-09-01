91c9edfaad5704f6cedf716730378d277026c68d
jim <jim19rogers@gmail.com>
2024-09-01 23:00:58 +0300

    advanced model , admin and settings

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,30 +1,26 @@
 from django.contrib import admin
-from .models import Book
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-
-
-
+from .models import Book #Import Book model from models
 
 # Register your models here.
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
 
+#admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ('title', 'author') # Filter books by title and author
+    search_fields = ('title', 'author')
+                     
 admin.site.register(Book, BookAdmin)
 
-#Create a custom ModelAdmin class to manage the additional fields in the Django admin.
-
-
+#Integrate the Custom User Model into Admin
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ('email', 'is_staff', 'date_of_birth', 'username', 'profile_photo')
 admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,48 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+from relationship_app.models import Book
+
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-#abrastract user
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-
-
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, date_of_birth=None, profile_photo=None, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
+        
+        #Validating Email
         if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+            raise ValueError('User must have an email address')
+        #Fetching and normalizing email
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        #Setting password (hashes password)
         user.set_password(password)
+        #saving created user in current database
+        user.save(using=self._db)
+        #returning created user
+        return user
+    
+    #creating superuser ensuring administrative users can be created with the required fields fields
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
-
-    def create_superuser(self, username, email, date_of_birth=None, profile_photo=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, date_of_birth, profile_photo, password, **extra_fields)
-
-#Link the custom user manager to your custom user model:
-
-
+    
+#Custom User Model by extending AbstractUser
+#from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    date_of_birth = models.DateField(default='2020-01-01', blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_photos', blank=True, null=True)
 
     objects = CustomUserManager()
+    def __str__(self):
+        return self.email
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DateField(default = '2020-01-01')
+    modified_by = models.ForeignKey()
+
+#Extending Book Model with Custom Permissions
+    class Meta:
+        permissions =(
+            ('can_create', 'Can create'),
+            ('can_view', 'Can view'),
+            ('can_edit', 'Can edit'),
+            ('can_delete', 'Can delete'),
+
+        )
+
+    def __str__(self):
+        return f"{self.title} by {self.author} published on {self.publication_date}"
 
 
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view'),
+            ('can_create', 'Can create'),
+            ('can_edit', 'Can edit'),
+            ('can_delete', 'Can delete'),
+        ]       
+          
+   
